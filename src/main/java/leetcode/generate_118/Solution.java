5f93bafc3e27a4f0fabb5a2c1eb85ddd32fa3c42
winwto <liuxuenbrw@163.com>
2019-02-16 17:30:04 +0800

    leetcode generate_118

---
+++ src/main/java/leetcode/generate_118/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.generate_118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> generate(int numRow) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if (numRow == 0) {
+            return res;
+        }
+        List<Integer> r1 = new ArrayList<>();
+        r1.add(1);
+        res.add(r1);
+        if (numRow == 1) {
+            return res;
+        }
+        List<Integer> r2 = new ArrayList<>();
+        r2.add(1);
+        r2.add(1);
+        res.add(r2);
+        if (numRow == 2) {
+            return res;
+        }
+        for (int i = 2; i < numRow; i++) {
+            List<Integer> temp = new ArrayList<>();
+            List<Integer> tab = res.get(i - 1);
+            temp.add(1);
+            for (int j = 0; j < i - 1; j++) {
+                temp.add(tab.get(j) + tab.get(j + 1));
+            }
+            temp.add(1);
+            res.add(temp);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/leetcode/generate_118/readMe.md
@@ -0,0 +1,9 @@
+### 杨辉三角
+#### 题意
+给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+
+![conv_ops](https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif)
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+#### 题解
+直接按照题目要求做就行，注意细节。
