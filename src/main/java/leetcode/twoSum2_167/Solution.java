e48cea2a1359775ef29f099a18f4c0d1d318e28d
winwto <liuxuenbrw@163.com>
2019-02-19 00:06:52 +0800

    leetcode twoSum2_167

--- src/main/java/leetcode/twoSum2_167/Solution.java
+++ src/main/java/leetcode/twoSum2_167/Solution.java
@@ -4,6 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
+    /**
+     * 与twoSum相同的解法
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -12,6 +18,27 @@ public class Solution {
                 return new int[]{map.get(wanted) + 1, i + 1};
             }
             map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    /**
+     * 因为数组是有序的，索引可以使用双索引进行碰撞查找
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                return new int[] {left + 1, right + 1};
+            } else if (nums[left] + nums[right] < target) {
+                left ++;
+            } else {
+                right --;
+            }
         }
         return null;
     }
