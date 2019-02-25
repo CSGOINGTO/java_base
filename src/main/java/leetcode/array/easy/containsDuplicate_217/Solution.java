e9916255255e15d3465c4d2ceef542c8e4bd728a
winwto <liuxuenbrw@163.com>
2019-02-25 15:33:35 +0800

    leetcode 存在重复元素_217

---
+++ src/main/java/leetcode/array/easy/containsDuplicate_217/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.array.easy.containsDuplicate_217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
