1bf22db0c64f78597b632ddafb3dbf364c8752f8
winwto <liuxuenbrw@163.com>
2019-03-19 21:17:45 +0800

    leetcode 找到所有数组中消失的数字_448

---
+++ src/main/java/leetcode/array/easy/findAllNumbersDisappearedInAnArray_448/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.array.easy.findAllNumbersDisappearedInAnArray_448;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            if (temp < 0) {
+                temp = -temp;
+            }
+            if (nums[temp - 1] < 0) {
+                nums[temp - 1] = -nums[temp - 1];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+}
