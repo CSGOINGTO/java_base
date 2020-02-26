a453013d1f85496fba846405c07b83c9ef96f658
winwto <liuxuenbrw@163.com>
2020-02-26 18:32:51 +0800

    整理项目

--- src/main/java/design_patterns/simpleFactory/ProductOne.java
+++
@@ -1,8 +0,0 @@
-package design_patterns.simpleFactory;
-
-public class ProductOne implements Product {
-    @Override
-    public String isFunction(String introduction) {
-        return "productionOne" + introduction;
-    }
-}
--- src/main/java/design_patterns/simpleFactory/ProductionTwo.java
+++
@@ -1,8 +0,0 @@
-package design_patterns.simpleFactory;
-
-public class ProductionTwo implements Product {
-    @Override
-    public String isFunction(String introduction) {
-        return "productionTwo" + introduction;
-    }
-}
