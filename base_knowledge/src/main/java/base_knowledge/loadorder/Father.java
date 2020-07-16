ffb7696d57ef75b1e95004b14375e32ed924cae3
winwto <liuxuenbrw@163.com>
2020-07-16 10:34:36 +0800

    Java类初始化顺序

---
+++ base_knowledge/src/main/java/base_knowledge/loadorder/Father.java
@@ -0,0 +1,42 @@
+package base_knowledge.loadorder;
+
+public class Father {
+
+    private String fatherName;
+
+    private static final String Age = "20";
+
+    /*
+    如果该类为父类，那么默认的无参构造器必须存在！
+     */
+    Father() {
+        System.out.println("我是父类无参构造器！");
+    }
+
+    Father(String fatherName) {
+        System.out.println("我是父类有参构造器！");
+        this.fatherName = fatherName;
+    }
+
+    /*
+    静态变量和静态代码块的执行看两者的先后顺序
+     */
+    private static final String ADDRESS = "address";
+
+    static {
+        System.out.println("我是父类静态代码块！");
+        System.out.println("父类Age： " + Age);
+        System.out.println("父类Address" + ADDRESS);
+    }
+
+    {
+        System.out.println("我是父类匿名代码块");
+    }
+
+    @Override
+    public String toString() {
+        return "Father{" +
+                "name='" + fatherName + '\'' + ", age='" + Age + '\'' +
+                '}';
+    }
+}
---
+++ base_knowledge/src/main/java/base_knowledge/loadorder/Son.java
@@ -0,0 +1,40 @@
+package base_knowledge.loadorder;
+
+public class Son extends Father{
+
+    private String sonName = "sonName";
+
+    /*
+    子类构造器会默认调用父类的无参构造器super()
+     */
+    Son() {
+        System.out.println("我是子类无参构造器！");
+    }
+
+    /*
+    构造器的加载顺序在静态代码、匿名代码块和全局变量之后，并且执行时会默认调用父类的无参构造器
+     */
+    Son(String sonName) {
+        System.out.println("我是子类有参构造器！");
+        this.sonName = sonName;
+    }
+
+    /*
+    匿名代码块和全局变量处于同一级加载顺序，当类中的静态代码执行完毕之后，按照先后顺序执行匿名代码块和全局变量
+     */
+    {
+        System.out.println("我是子类匿名代码块！");
+        System.out.println("我是子类匿名代码块中的sonName：" + sonName);
+    }
+
+    static {
+        System.out.println("我是子类静态代码块！");
+    }
+
+    @Override
+    public String toString() {
+        return "Son{" +
+                "sonName='" + sonName + '\'' +
+                '}';
+    }
+}
