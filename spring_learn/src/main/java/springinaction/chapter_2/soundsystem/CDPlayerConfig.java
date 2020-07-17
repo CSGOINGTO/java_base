cb1fab3e45ab30fb7494ef6c1b75db61f056a63b
winwto <liuxuenbrw@163.com>
2020-07-17 10:41:22 +0800

    Spring In Action 第二章（装配bean）

--- spring_learn/src/main/java/springinaction/chapter_2/soundsystem/CDPlayerConfig.java
+++ spring_learn/src/main/java/springinaction/chapter_2/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,22 @@
 package springinaction.chapter_2.soundsystem;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class CDPlayerConfig {
+
+    @Bean
+    public CompactDisc sgtPeppers() {
+        System.out.println("我是CDPlayerConfig中的sgtPeppers方法");
+        return new SgtPeppers();
+    }
+
+
+    @Bean
+    public CDPlayer cdPlayer() {
+        return new CDPlayer(sgtPeppers());
+    }
 }
--- spring_learn/src/test/java/springinaction/chapter_2/soundsystem/CdPlayerTest.java
+++ spring_learn/src/test/java/springinaction/chapter_2/soundsystem/CdPlayerTest.java
@@ -3,6 +3,7 @@ package springinaction.chapter_2.soundsystem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -13,8 +14,17 @@ public class CdPlayerTest {
     @Autowired
     private CompactDisc cd;
 
+    @Autowired
+    @Qualifier("CDPlayer")
+    private MediaPlayer player;
+
     @Test
     public void cdShouldNotBeNull() {
-        cd.play();
+        assert cd != null;
+    }
+
+    @Test
+    public void play() {
+        player.play();
     }
 }
