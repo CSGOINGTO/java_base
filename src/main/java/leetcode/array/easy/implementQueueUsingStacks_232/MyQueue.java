d7129b3850052bf309b392e7ddac3ad875e814c5
winwto <liuxuenbrw@gmail.com>
2020-11-18 16:38:00 +0800

    用栈实现队列_232

--- src/main/java/leetcode/array/easy/implementQueueUsingStacks_232/MyQueue.java
+++ src/main/java/leetcode/array/easy/implementQueueUsingStacks_232/MyQueue.java
@@ -3,7 +3,7 @@ package leetcode.array.easy.implementQueueUsingStacks_232;
 import java.util.ArrayList;
 import java.util.List;
 
-class MyQueue {
+public class MyQueue {
 
     private List<Integer> popList;
 
@@ -14,12 +14,6 @@ class MyQueue {
     private int popIndex;
 
     private int pushIndex;
-import java.util.Stack;
-
-public class MyQueue {
-    private final Stack<Integer> stack_help = new Stack<>();
-
-    private final Stack<Integer> stack_queue = new Stack<>();
 
     /**
      * Initialize your data structure here.
@@ -80,39 +74,3 @@ public class MyQueue {
  * boolean param_4 = obj.empty();
  */
 
-
-public void push(int x) {
-    stack_help.push(x);
-}
-
-    /**
-     * Removes the element from in front of queue and returns that element.
-     */
-    public int pop() {
-        if (stack_queue.isEmpty()) {
-            while (!stack_help.isEmpty()) {
-                stack_queue.push(stack_help.pop());
-            }
-        }
-        return stack_queue.pop();
-
-    }
-
-    /**
-     * Get the front element.
-     */
-    public int peek() {
-        if (stack_queue.isEmpty()) {
-            while (!stack_help.isEmpty()) {
-                stack_queue.push(stack_help.pop());
-            }
-        }
-        return stack_queue.peek();
-    }
-
-    /**
-     * Returns whether the queue is empty.
-     */
-    public boolean empty() {
-        return stack_help.isEmpty() && stack_queue.isEmpty();
-    }
--- src/main/java/leetcode/array/easy/implementQueueUsingStacks_232/readMe.md
+++ src/main/java/leetcode/array/easy/implementQueueUsingStacks_232/readMe.md
@@ -1,21 +1,7 @@
-### 思路
-1. 
+### 思路一
 + 使用两个List分别代表push后的队列和pop后的队列，使用pushIndex表示push后队列的位置；使用popIndex表示pop后队列的位置，使用size表示队列中元素的个数
 + 每次push时，pushIndex+1，size+1
 + 每次pop时，popIndex+1，size-1
-
-
-2. 
----
-- stack_help： 1->2->3->4 ... empty ... 5->6
-- stack_queue：4->3->2->1 ... empty ... 6->5
----
-
-+ 使用栈stack_help存储每次push进来的元素，使用另外一个栈stack_queue模拟队列的操作
-+ 每次push元素时都进入到stack_help
-+ 每次pop元素时，首先判断stack_queue中是否存在元素，如果不存在元素，则直接将stack_help中的元素全部pop出来放入到stack_queue中，然后再从stack_queue中pop；
-否则直接从stack_help中pop
-+ 每次peek元素时，原理同pop操作，只是将最后的pop修改为peek。
 
 ### 代码
 ```java
@@ -72,6 +58,28 @@ class MyQueue {
 }
 ```
 
+### 复杂度
++ 时间复杂度：O(1)
++ 空间复杂度：O(n)
+
+***
+***
+***
+
+### 思路二
+
+---
+- stack_help： 1->2->3->4 ... empty ... 5->6
+- stack_queue：4->3->2->1 ... empty ... 6->5
+---
+
++ 使用栈stack_help存储每次push进来的元素，使用另外一个栈stack_queue模拟队列的操作
++ 每次push元素时都进入到stack_help
++ 每次pop元素时，首先判断stack_queue中是否存在元素，如果不存在元素，则直接将stack_help中的元素全部pop出来放入到stack_queue中，然后再从stack_queue中pop；
+否则直接从stack_help中pop
++ 每次peek元素时，原理同pop操作，只是将最后的pop修改为peek。
+
+
 ```java
 public class MyQueue {
     private final Stack<Integer> stack_help = new Stack<>();
@@ -125,13 +133,7 @@ public class MyQueue {
 
 }
 ```
-
 ### 复杂度
-1. 
-+ 时间复杂度：O(1)
-+ 空间复杂度：O(n)
-
-2. 
 + 时间复杂度：
     + push()：O(1)
     + pop()：O(n)
