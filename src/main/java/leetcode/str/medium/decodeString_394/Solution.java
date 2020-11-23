f2915fb28dfe1ce8aa2113b09d53809aac621f25
winwto <liuxuenbrw@gmail.com>
2020-11-23 16:01:04 +0800

    字符串解码_394

--- src/main/java/leetcode/str/medium/decodeString_394/Solution.java
+++ src/main/java/leetcode/str/medium/decodeString_394/Solution.java
@@ -6,144 +6,41 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.decodeString1("abc3[cd]xyz"));
+        System.out.println(solution.decodeString3("3[a2[c]]"));
     }
 
     public String decodeString(String s) {
-        StringBuilder resStr = new StringBuilder();
-        String midStr = "";
+        StringBuilder resSb = new StringBuilder();
         Stack<Integer> stringNums = new Stack<>();
         Stack<String> stringStack = new Stack<>();
         char[] s_chars = s.toCharArray();
         int num = 0;
         for (int i = 0; i < s_chars.length; i++) {
             if (s_chars[i] >= '0' && s_chars[i] <= '9') {
-                num = num * 10 + s_chars[i] - '0';
-            } else if (s_chars[i] == '[') {
-                if (!"".equals(midStr)) {
-                    stringStack.push(midStr);
-                }
-                stringNums.push(num);
-                midStr = "";
-                num = 0;
-            } else if ((s_chars[i] >= 'A' && s_chars[i] <= 'Z') || (s_chars[i] >= 'a' && s_chars[i] <= 'z')) {
-                midStr += s_chars[i];
-            } else if (s_chars[i] == ']') {
-                if (!"".equals(midStr)) {
-                    int nums = stringNums.pop();
-                    String temp = midStr;
-                    for (int j = 0; j < nums - 1; j++) {
-                        midStr += temp;
-                    }
-                    if (!stringStack.empty()) {
-                        stringStack.push(stringStack.pop() + midStr);
-                    }
-                    if (stringNums.empty() && stringStack.empty()) {
-                        resStr.append(midStr);
-                    } else if (stringNums.empty() && !stringStack.empty()) {
-                        resStr.append(stringStack.pop());
-                    }
-                    midStr = "";
-                } else {
-                    String tempString = stringStack.pop();
-                    String temp = "";
-                    int nums = stringNums.pop();
-                    for (int j = 0; j < nums; j++) {
-                        temp += tempString;
-                    }
-                    if (!stringStack.empty()) {
-                        stringStack.push(stringStack.pop() + temp);
-                    }
-                    if (stringNums.empty()) {
-                        resStr.append(temp);
-                    }
-                }
-            }
-        }
-        String res = resStr.toString();
-//        while (!stringStack.isEmpty()) {
-//            String temp = stringStack.pop();
-//            if (!"".equals(temp)) {
-//                res += temp;
-//            }
-//        }
-        return res;
-    }
-
-    public String decodeString2(String s) {
-        char[] s_chars = s.toCharArray();
-        Stack<String> stringStack = new Stack<>();
-        Stack<Integer> numStack = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        int matchNum = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s_chars[i] > '0' && s_chars[i] <= '9') {
-                int num = s_chars[i] - '0';
-                while (s_chars[++i] >= '0' && s_chars[i] <= '9') {
+                for (; i < s_chars.length && (s_chars[i] >= '0' && s_chars[i] <= '9'); i++) {
                     num = num * 10 + s_chars[i] - '0';
                 }
                 i--;
-                numStack.push(num);
-            }
-            if ((s_chars[i] >= 'a' && s_chars[i] <= 'z') || (s_chars[i] >= 'A' && s_chars[i] <= 'Z')) {
-                sb.append(s_chars[i]);
-                while (i++ < s.length() && (s_chars[i] >= 'a' && s_chars[i] <= 'z') || (s_chars[i] >= 'A' && s_chars[i] <= 'Z')) {
-                    sb.append(s_chars[i]);
-                }
-                i--;
-                if (matchNum > 1) {
-                    stringStack.push(stringStack.pop() + sb.toString());
-                } else {
-                    stringStack.push(sb.toString());
-                }
-                sb = new StringBuilder();
-            }
-            if (s_chars[i] == '[') {
-                matchNum++;
-            }
-            if (s_chars[i] == ']') {
-                if (!numStack.isEmpty() && !stringStack.isEmpty()) {
-                    matchNum--;
-                    String temp = stringStack.pop();
-                    int num = numStack.pop();
-                    for (int j = 0; j < num; j++) {
-                        sb.append(temp);
+            } else if ((s_chars[i] >= 'A' && s_chars[i] <= 'Z') || (s_chars[i] >= 'a' && s_chars[i] <= 'z')) {
+                resSb.append(s_chars[i]);
+            } else if (s_chars[i] == '[') {
+                stringNums.push(num);
+                stringStack.push(resSb.toString());
+                num = 0;
+                resSb = new StringBuilder();
+            } else {
+                int numTemp = stringNums.pop();
+                String stringTemp = resSb.toString();
+                if (stringTemp.length() > 0) {
+                    StringBuilder sbTemp = new StringBuilder();
+                    for (int j = 0; j < numTemp; j++) {
+                        sbTemp.append(stringTemp);
                     }
-                    if (!stringStack.isEmpty()) {
-                        stringStack.push(stringStack.pop() + sb.toString());
-                    } else {
-                        stringStack.push(sb.toString());
-                    }
-                    sb = new StringBuilder();
+                    resSb = new StringBuilder(stringStack.pop() + sbTemp.toString());
                 }
-//                if (!sb.toString().equals("")) {
-//                    String temp = sb.toString();
-//                    int num = numStack.pop();
-//                    for (int j = 1; j < num; j++) {
-//                        sb.append(temp);
-//                    }
-//                    if (!stringStack.isEmpty()) {
-//                        stringStack.push(stringStack.pop() + sb.toString());
-//                    } else {
-//                        stringStack.push(sb.toString());
-//                    }
-//                    sb = new StringBuilder();
-//                }
             }
         }
-        if (!numStack.isEmpty()) {
-            int num = numStack.pop();
-            StringBuilder sbTemp = new StringBuilder();
-            String temp = stringStack.pop();
-            for (int i = 0; i < num; i++) {
-                sbTemp.append(temp);
-            }
-            return sbTemp.append(sb.toString()).toString();
-        }
-        if (!stringStack.isEmpty()) {
-            return stringStack.pop() + sb.toString();
-        }
-        return sb.toString();
+        return resSb.toString();
     }
 
     public String decodeString1(String s) {
@@ -175,6 +72,43 @@ public class Solution {
             }
         }
         return res.toString();
+    }
+
+    /**
+     * 递归版本
+     * 注意：
+     * 1. 需要将当前遍历到的位置作为参数传入
+     * 2. 其他操作与使用栈的解法相同
+     */
+    public String decodeString3(String s) {
+        return dfs(s, 0)[0];
+    }
+
+    private String[] dfs(String s, int index) {
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
+        char[] s_chars = s.toCharArray();
+        for (; index < s.length(); index++) {
+            if (s_chars[index] >= '0' && s_chars[index] <= '9') {
+                num = num * 10 + s_chars[index] - '0';
+            } else if ((s_chars[index] >= 'a' && s_chars[index] <= 'z') || (s_chars[index] >= 'A' && s_chars[index] <= 'Z')) {
+                sb.append(s_chars[index]);
+            } else if (s_chars[index] == '[') {
+                String[] dfs = dfs(s, ++index);
+                String dfsRes = dfs[0];
+                // 将index重置为已经遍历到的位置，防止重复遍历
+                index = Integer.parseInt(dfs[1]);
+                StringBuilder tempSb = new StringBuilder();
+                while (num > 0) {
+                    tempSb.append(dfsRes);
+                    num--;
+                }
+                sb.append(tempSb.toString());
+            } else {
+                return new String[]{sb.toString(), String.valueOf(index)};
+            }
+        }
+        return new String[]{sb.toString(), String.valueOf(0)};
     }
 
 }
