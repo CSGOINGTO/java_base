ddfd16ca76b2fd5c8840655ed1b3d164cec01351
winwto <liuxuenbrw@gmail.com>
2020-12-29 13:58:17 +0800

    Revert "前 K 个高频元素_347"
    This reverts commit 658b68ba

--- src/main/java/leetcode/array/easy/shortestDistanceToACharacter_821/Solution.java
+++ src/main/java/leetcode/array/easy/shortestDistanceToACharacter_821/Solution.java
@@ -1,41 +1,11 @@
 package leetcode.array.easy.shortestDistanceToACharacter_821;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(Arrays.toString(solution.shortestToChar("loveleetcode", 'e')));
-    }
-
     public int[] shortestToChar(String S, char C) {
-        if (S == null || "".equals(S)) {
-            return new int[0];
-        }
-        int[] res = new int[S.length()];
-        char[] S_chars = S.toCharArray();
-        for (int i = 0; i < S_chars.length; i++) {
-            int len = Integer.MAX_VALUE;
-            int tempLen = 0;
-            for (int j = 0; j < S_chars.length; j++) {
-                if (i != j && S_chars[j] == C) {
-                    len = Math.min(len, tempLen);
-//                    tempLen = 0;
-                } else if (i == j) {
-                    tempLen = 0;
-                } else {
-                    tempLen++;
-                }
-            }
-            res[i] = len;
-        }
-        return res;
-    }
-
-    public int[] shortestToChar1(String S, char C) {
         List<Integer> c_index = new ArrayList<>();
         int[] res = new int[S.length()];
         for (int i = 0; i < S.length(); i++) {
