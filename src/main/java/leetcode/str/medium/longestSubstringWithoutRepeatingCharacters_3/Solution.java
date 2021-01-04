4109979716805a3e2cb0f85f371f34b4d01b2390
winwto <liuxuenbrw@gmail.com>
2021-01-04 17:51:10 +0800

    无重复字符的最长子串_3

--- src/main/java/leetcode/str/medium/longestSubstringWithoutRepeatingCharacters_3/Solution.java
+++ src/main/java/leetcode/str/medium/longestSubstringWithoutRepeatingCharacters_3/Solution.java
@@ -78,4 +78,24 @@ public class Solution {
         }
         return Math.max(maxLen, len);
     }
+
+    public int lengthOfLongestSubstring3(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring3("dvdf"));
+    }
 }
