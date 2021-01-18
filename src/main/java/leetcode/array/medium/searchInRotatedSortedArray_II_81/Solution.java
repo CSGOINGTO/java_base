fd3eb865fcbade8fcc5a229f7927b088b9c751be
winwto <liuxuenbrw@gmail.com>
2021-01-18 16:52:11 +0800

    搜索旋转排序数组 II_81

--- src/main/java/leetcode/array/medium/searchInRotatedSortedArray_II_81/Solution.java
+++ src/main/java/leetcode/array/medium/searchInRotatedSortedArray_II_81/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.search(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1}, 2));
+        System.out.println(solution.search1(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1}, 2));
     }
 
     public boolean search(int[] nums, int target) {
@@ -12,34 +12,68 @@ public class Solution {
         int left = 0;
         int right = nums.length - 1;
         while (left < right) {
+            // 向下取整
             int mid = (left + right) >>> 1;
             if (nums[mid] == target) return true;
-            // [mid, right]为递增区间
-            if (nums[mid] <= nums[right]) {
-                // target比[left, mid]区间值大，说明在[mid + 1, right]中
-                if (nums[right] < target) {
-                    left = mid + 1;
-                    continue;
-                }
-                if (nums[mid] >= target && nums[right] <= target) {
-                    left = mid + 1;
-                } else {
+            // 1. 判断在[left, mid - 1]还是[mid, right]区间中
+            // target在[left, mid - 1]区间 [left, mid]区间递增
+            if (nums[left] < nums[mid]) {
+                // 保证target在[left, mid - 1]区间内
+                if (target < nums[mid]) {
                     right = mid - 1;
+                    // 否则target在[mid, right]区间内
+                } else {
+                    left = mid;
                 }
-                // [left, mid]为递增区间
+                // target在[mid + 1, right]区间内
             } else {
-                // target不在[mid, right]区间内大，说明在[left, mid - 1]中
-                if (nums[mid] < target) {
-                    right = mid - 1;
-                    continue;
-                }
-                if (nums[left] <= target && nums[mid] >= target) {
-                    right = mid - 1;
-                } else {
+                if (target < nums[right]) {
                     left = mid + 1;
+                } else {
+                    right = mid;
                 }
             }
+            System.out.println("left: " + left + " right: " + right);
         }
+        return nums[left] == target;
+    }
+
+    public boolean search1(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return false;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            // 向下取整，当只有两个
+            int mid = (left + right) >>> 1;
+            // [left, mid - 1]
+            if (nums[mid] > nums[left]) {
+                // [left, target, mid]
+                if (nums[left] <= target && target <= nums[mid]) {
+                    // 落在前有序数组里
+                    right = mid;
+                    // [mid + 1, target, right]
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < nums[left]) {
+                // 让分支和上面分支一样
+                // [mid + 1, target, right]
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            } else {
+                // 要排除掉左边界之前，先看一看左边界可以不可以排除
+                if (nums[left] == target) {
+                    return true;
+                } else {
+                    left = left + 1;
+                }
+            }
+            System.out.println("left: " + left + " right: " + right);
+        }
+        // 后处理，夹逼以后，还要判断一下，是不是 target
         return nums[left] == target;
     }
 }
