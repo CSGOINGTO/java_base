ab784941260f569c869d12e2bd12b22817e14898
winwto <liuxuenbrw@gmail.com>
2021-01-19 10:25:11 +0800

    删除排序数组中的重复项_26

--- src/main/java/leetcode/array/easy/removeDuplicates_26/Solution.java
+++ src/main/java/leetcode/array/easy/removeDuplicates_26/Solution.java
@@ -1,8 +1,14 @@
 package leetcode.array.easy.removeDuplicates_26;
 
 public class Solution {
-    public int removeDuplicates (int[] nums) {
-        if(nums == null || nums.length == 0) {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.removeDuplicates2(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         int len = 0;
@@ -12,5 +18,43 @@ public class Solution {
             }
         }
         return ++len;
+    }
+
+    public int removeDuplicates1(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int res = 1;
+        for (int left = 0; left < nums.length; left++) {
+            int repeatLen = 0;
+            for (int right = left + 1; right < nums.length; right++) {
+                if (nums[left] == nums[right]) {
+                    repeatLen++;
+                    continue;
+                }
+                if (right == nums.length - 1 && nums[left] == nums[right]) {
+                    repeatLen++;
+                    nums[res] = nums[right];
+                    res ++;
+                    left += repeatLen;
+                    break;
+                }
+                nums[res] = nums[right];
+                res ++;
+                left += repeatLen;
+                break;
+            }
+        }
+        return res;
+    }
+
+    public int removeDuplicates2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int left = 0;
+        for (int right = left; right < nums.length; right++) {
+            if (nums[left] != nums[right]) {
+                if (left != right) nums[++left] = nums[right];
+                left++;
+            }
+        }
+        return left + 1;
     }
 }
