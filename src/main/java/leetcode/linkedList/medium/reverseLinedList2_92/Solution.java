f3963723214b20140a89c1a54cdccaaf3211ec0a
winwto <liuxuenbrw@gmail.com>
2021-01-21 17:35:12 +0800

    反转链表II_92

--- src/main/java/leetcode/linkedList/medium/reverseLinedList2_92/Solution.java
+++ src/main/java/leetcode/linkedList/medium/reverseLinedList2_92/Solution.java
@@ -28,4 +28,30 @@ public class Solution {
         res.next = lastNode;
         return res1.next;
     }
+
+
+    public ListNode reverseBetween1(ListNode head, int m, int n) {
+        ListNode res = head;
+        // 1->2->3->4->5->6
+        // m = 2, n = 4
+        for (int i = 0; i < m; i++) {
+            // head = 2
+            head = head.next;
+        }
+        int i = 0;
+        ListNode node = null;
+        while (i < n - m) {
+            // 3
+            ListNode tmpNode = head.next;
+            // 1-3-2
+            head.next = node;
+            node = head;
+            head = tmpNode;
+            i++;
+        }
+        if (head != null) {
+            node.next = head.next;
+        }
+        return res;
+    }
 }
