fab0d7092a05d5196975feb91a5cce004e3b1fad
CSGOINGTO <liuxuenbrw@163.com>
2021-01-23 09:32:36 +0800

    翻转链表II_92

--- src/main/java/leetcode/linkedList/medium/reverseLinedList2_92/Solution.java
+++ src/main/java/leetcode/linkedList/medium/reverseLinedList2_92/Solution.java
@@ -29,29 +29,50 @@ public class Solution {
         return res1.next;
     }
 
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = null;
+        Solution solution = new Solution();
+        System.out.println(solution.reverseBetween1(l1, 3, 5));
+    }
 
     public ListNode reverseBetween1(ListNode head, int m, int n) {
-        ListNode res = head;
-        // 1->2->3->4->5->6
-        // m = 2, n = 4
-        for (int i = 0; i < m; i++) {
-            // head = 2
+        /*
+            1. 设置一个头结点resHead，其next为head，同时设置另外一个新的节点doHead（具体干活的节点）与resHead指向相同的对象。
+            2. 将head遍历到第m个节点startHead，将m--n中的节点进行翻转。（在遍历的同时，将head指向下一个节点，遍历完之后，head指向的是原链表中第n个节点的下一个节点）
+            3. 将doHead遍历到第n个节点处，并将其下一个节点指向head。
+            4. 返回resHead的next节点
+         */
+
+        ListNode resHead = new ListNode(0);
+        resHead.next = head;
+        ListNode doHead = resHead;
+        for (int i = 1; i < m; i++) {
             head = head.next;
+            doHead = doHead.next;
         }
-        int i = 0;
-        ListNode node = null;
-        while (i < n - m) {
-            // 3
+        ListNode startHead = null;
+        for (int i = 1; i <= n - m + 1; i++) {
             ListNode tmpNode = head.next;
-            // 1-3-2
-            head.next = node;
-            node = head;
+            head.next = startHead;
+            startHead = head;
             head = tmpNode;
-            i++;
         }
-        if (head != null) {
-            node.next = head.next;
+//        doHead = doHead.next;
+        doHead.next = startHead;
+        // doHead头结点，遍历到当前head的前一个节点
+        for (int i = 1; i <= n - m + 1; i++) {
+            doHead = doHead.next;
         }
-        return res;
+        doHead.next = head;
+        return resHead.next;
     }
 }
