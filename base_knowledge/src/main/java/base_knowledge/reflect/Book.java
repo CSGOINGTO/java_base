06f3de9791a7ffb3fdc5807373f30a86a5099d87
winwto <liuxuenbrw@gmail.com>
2021-02-09 17:53:09 +0800

    反射对象

---
+++ base_knowledge/src/main/java/base_knowledge/reflect/Book.java
@@ -0,0 +1,56 @@
+package base_knowledge.reflect;
+
+public class Book {
+    private final static String TAG = "BookTag";
+
+    private String name;
+    private String author;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public Book() {
+    }
+
+    private Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private String declaredMethod(int index) {
+        String string = null;
+        switch (index) {
+            case 0:
+                string = "I am declaredMethod 1 !";
+                break;
+            case 1:
+                string = "I am declaredMethod 2 !";
+                break;
+            default:
+                string = "I am declaredMethod 1 !";
+        }
+
+        return string;
+    }
+}
