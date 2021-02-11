60032849855ba51005dd92c18b45c8a25e131c20
CSGOINGTO <liuxuenbrw@163.com>
2021-02-11 10:23:58 +0800

    最长有效括号_32

--- src/main/java/leetcode/str/hard/longestValidParentheses_32/Solution.java
+++ src/main/java/leetcode/str/hard/longestValidParentheses_32/Solution.java
@@ -6,17 +6,16 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.longestValidParentheses("()(())"));
+        System.out.println(solution.longestValidParentheses(")()())"));
     }
 
-
-    public int longestValidParentheses(String s) {
+    public int longestValidParentheses1(String s) {
         if (s == null || s.length() == 0) return 0;
         char[] s_chars = s.toCharArray();
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);
         int maxLen = 0;
-        for (int i = 0; i < s_chars.length; i++) {
+        for (int i = 0; i < s.length(); i++) {
             if (s_chars[i] == '(') {
                 stack.push(i);
             } else {
@@ -28,7 +27,38 @@ public class Solution {
                 }
             }
         }
+        return maxLen;
+    }
 
+    public int longestValidParentheses(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int maxLen = 0;
+        int[] dp = new int[s.length()];
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                dp[i] = 0;
+            } else {
+                // 前一个位置是(
+                if (s.charAt(i - 1) == '(') {
+                    if (i - 2 >= 0) {
+                        dp[i] = dp[i - 2] + 2;
+                    } else {
+                        dp[i] = 2;
+                    }
+                    // 前一个位置是)
+                } else {
+                    // 判断与当前)对应位置是否是(
+                    if ((i - dp[i - 1] - 1) >= 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                        dp[i] = dp[i - 1] + 2;
+                        // 加上对应位置之前的匹配长度
+                        if (i - dp[i - 1] - 2 >= 0) {
+                            dp[i] = dp[i] + dp[i - dp[i - 1] - 2];
+                        }
+                    }
+                }
+                maxLen = Math.max(maxLen, dp[i]);
+            }
+        }
         return maxLen;
     }
 }
