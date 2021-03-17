cc4b37d855269017547e6236f6faed75d6cabf1f
winwto <liuxuenbrw@gmail.com>
2021-03-17 12:36:02 +0800

    HashMapDemo

---
+++ base_knowledge/src/main/java/base_knowledge/collection/HashMapDemo.java
@@ -0,0 +1,53 @@
+package base_knowledge.collection;
+
+import base_knowledge.sourcecode.java.util.HashMap;
+import base_knowledge.sourcecode.java.util.Map;
+
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        Map<MyObject, String> map = new HashMap<>();
+        for (int i = 0; i < 20; i++) {
+            map.put(new MyObject(String.valueOf(i)), String.valueOf(i));
+        }
+        System.out.println(map.get(new MyObject(String.valueOf(1))));
+    }
+
+}
+
+
+class MyObject{
+    private String name;
+
+    public MyObject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "MyObject{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyObject myObject = (MyObject) o;
+        return name.equals(myObject.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+}
